37430d508034df898331775055eee4095736d771
CarlitosSteven <carlosesteb2000@hotmail.com>
2024-11-08 17:48:45 -0600

    Creando proyecto Spring

---
+++ src/test/java/com/apiescolar/SistemaEscolar/SistemaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiescolar.SistemaEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

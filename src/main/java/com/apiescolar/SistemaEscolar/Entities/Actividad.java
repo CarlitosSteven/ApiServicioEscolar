6e734b44fc0eea0aad31d3d772e10f7442eaf077
CarlitosSteven <carlosesteb2000@hotmail.com>
2024-11-09 20:00:00 -0600

    Resolviendo bug de ambiguedad en base de datos con Actvidades
    -Se modifico la tabla de la base de datos "actividades" para agregar una
    constante unique con (nombre, materia_id)
    
    -Se modifico la entidad Actividad.java para que reconociera la constante
    unique con (nombre, materia)

--- src/main/java/com/apiescolar/SistemaEscolar/Entities/Actividad.java
+++ src/main/java/com/apiescolar/SistemaEscolar/Entities/Actividad.java
@@ -10,10 +10,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Entity
-@Table(name = "actividades")
+@Table(name = "actividades", uniqueConstraints = @UniqueConstraint(columnNames = {"nombre", "materia_id"}))
 @Data
 public class Actividad {
 
